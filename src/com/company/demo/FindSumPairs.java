09eb2678ae8c8e8fdd98cfcff688066d71105f49
kabildeveloper <kabildeveloper@gmail.com>
2021-04-17 16:48:20 +0530

    10 changed to n

--- src/com/company/demo/FindSumPairs.java
+++ src/com/company/demo/FindSumPairs.java
@@ -7,21 +7,21 @@ public class FindSumPairs {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 if (j< arr[i].length-1 && arr[i][j] + arr[i][j+1] == n ) {
-                    System.out.println(arr[i][j] + " " + arr[i][j+1]);
+                    System.out.println(arr[i][j] + " + " + arr[i][j+1]+" => "+n);
                 }
 
                 if (i< arr.length-1 && arr[i][j] + arr[i+1][j] == n) {
-                    System.out.println(arr[i][j] + " " + arr[i+1][j]);
+                    System.out.println(arr[i][j] + " + " + arr[i+1][j]+" => "+n);
                 }
 
                 if(i< arr.length-1 && j< arr[i].length-1 && arr[i][j]+ arr[i+1][j+1] == n )
                 {
-                    System.out.println(arr[i][j]+" "+ arr[i+1][j+1]);
+                    System.out.println(arr[i][j]+" + "+ arr[i+1][j+1]+" => "+n);
                 }
 
                 if(i< arr.length-1 && j>0 && arr[i][j]+ arr[i+1][j-1] == n)
                 {
-                    System.out.println(arr[i][j]+" "+ arr[i-1][j-1]);
+                    System.out.println(arr[i][j]+" + "+ arr[i-1][j-1]+" => "+n);
                 }
 
             }
